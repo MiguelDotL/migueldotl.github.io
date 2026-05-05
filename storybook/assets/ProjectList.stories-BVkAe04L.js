93b649b52ea28610ef248bb377b3c587cc02273a
MiguelDotL <miguel.d.lozano@gmail.com>
2026-05-05 17:47:33 -0400

    deploy: reverse sb gradient (purple-to-pink) for continuity with gh

---
+++ storybook/assets/ProjectList.stories-BVkAe04L.js
@@ -0,0 +1,57 @@
+import{n as e}from"./chunk-DnJy8xQt.js";import{t}from"./jsx-runtime-BuQTzuGm.js";import{n,t as r}from"./esm-CVWkqyVS.js";import{t as i}from"./Projects-SBCY6n6L.js";import{n as a,t as o}from"./trim-agency-512-Bq6lghTE.js";import{i as s,n as c,r as l,t as u}from"./filthy-food-512-ffS-MWtm.js";import{a as d,i as f,n as p,o as m,r as h,t as g}from"./general-provision-512-VDpmbSQD.js";var _,v,y,b,x,S,C,w;e((()=>{r(),m(),i(),o(),s(),c(),f(),p(),_=t(),v=[{title:`T R I M Agency`,description:`Web Development`,imageURL:a,url:`//www.trimagency.com/`},{title:`C Solutions`,description:`Web Development`,imageURL:l,url:`//csolutions-us.com/`},{title:`Filthy Food`,description:`Web Development`,imageURL:u,url:`//filthyfood.com/`}],y={title:`Components/Composites/ProjectList`,component:d,decorators:[e=>(0,_.jsx)(`section`,{className:`projects`,style:{background:`var(--almost-black, #0c0c0c)`,padding:`3rem`},children:(0,_.jsx)(n.Container,{id:`story-tabs`,defaultActiveKey:`client`,children:(0,_.jsx)(n.Content,{children:(0,_.jsx)(e,{})})})})],parameters:{layout:`fullscreen`,docs:{description:{component:`Grid of basic project cards. Used in the Client Projects tab.`}}},argTypes:{projects:{control:{type:`object`},description:`Array of projects to render as cards`}}},b={args:{projects:v}},x={args:{projects:[v[0]]}},S={args:{projects:[{title:`T R I M Agency`,description:`Web Development`,imageURL:a,url:`//www.trimagency.com/`},{title:`C Solutions`,description:`Web Development`,imageURL:l,url:`//csolutions-us.com/`},{title:`Federated Insurance`,description:`Web Development`,imageURL:h,url:`//www.federated.ca/`},{title:`Filthy Food`,description:`Ecommerce`,imageURL:u,url:`//filthyfood.com/`},{title:`General Provision`,description:`Web Development`,imageURL:g,url:`//generalprovision.com/`}]}},C={args:{projects:[{title:`Some Project With An Unusually Long Title`,description:`Web Development`,imageURL:a,url:`#`},{title:`Another Long Project Title For Wrap Testing`,description:`Ecommerce + Backend Engineering`,imageURL:l,url:`#`},{title:`A Third One With A Lot Of Words`,description:`Web Development`,imageURL:u,url:`#`}]}},b.parameters={...b.parameters,docs:{...b.parameters?.docs,source:{originalSource:`{
+  args: {
+    projects: sampleProjects
+  }
+}`,...b.parameters?.docs?.source}}},x.parameters={...x.parameters,docs:{...x.parameters?.docs,source:{originalSource:`{
+  args: {
+    projects: [sampleProjects[0]!]
+  }
+}`,...x.parameters?.docs?.source}}},S.parameters={...S.parameters,docs:{...S.parameters?.docs,source:{originalSource:`{
+  args: {
+    projects: [{
+      title: 'T R I M Agency',
+      description: 'Web Development',
+      imageURL: trimAgency,
+      url: '//www.trimagency.com/'
+    }, {
+      title: 'C Solutions',
+      description: 'Web Development',
+      imageURL: cSolutions,
+      url: '//csolutions-us.com/'
+    }, {
+      title: 'Federated Insurance',
+      description: 'Web Development',
+      imageURL: federated,
+      url: '//www.federated.ca/'
+    }, {
+      title: 'Filthy Food',
+      description: 'Ecommerce',
+      imageURL: filthyFood,
+      url: '//filthyfood.com/'
+    }, {
+      title: 'General Provision',
+      description: 'Web Development',
+      imageURL: generalProvision,
+      url: '//generalprovision.com/'
+    }]
+  }
+}`,...S.parameters?.docs?.source}}},C.parameters={...C.parameters,docs:{...C.parameters?.docs,source:{originalSource:`{
+  args: {
+    projects: [{
+      title: 'Some Project With An Unusually Long Title',
+      description: 'Web Development',
+      imageURL: trimAgency,
+      url: '#'
+    }, {
+      title: 'Another Long Project Title For Wrap Testing',
+      description: 'Ecommerce + Backend Engineering',
+      imageURL: cSolutions,
+      url: '#'
+    }, {
+      title: 'A Third One With A Lot Of Words',
+      description: 'Web Development',
+      imageURL: filthyFood,
+      url: '#'
+    }]
+  }
+}`,...C.parameters?.docs?.source}}},w=[`ThreeProjects`,`OneProject`,`FiveProjects`,`LongTitlesGrid`]}))();export{S as FiveProjects,C as LongTitlesGrid,x as OneProject,b as ThreeProjects,w as __namedExportsOrder,y as default};
